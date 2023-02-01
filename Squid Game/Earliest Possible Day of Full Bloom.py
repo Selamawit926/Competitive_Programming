af83f5095aff67cb41422e14606213285b7439f0
Selamawit926 <se.selamawit.elias@gmail.com>
2023-02-01 19:49:30 +0300

    Squid_Game_FEB_01

--- Squid Game/Earliest Possible Day of Full Bloom.py
+++ Squid Game/Earliest Possible Day of Full Bloom.py
@@ -1,10 +1,21 @@
 class Solution:
     def earliestFullBloom(self, plantTime: List[int], growTime: List[int]) -> int:
+        # lst=[]
+        # days=0
+        # for i in range(len(plantTime)):
+        #     lst.append((growTime[i],plantTime[i]))
+        # lst.sort()
+        # for i in range(len(lst)):
+        #     days=max(days,lst[i][0])+lst[i][1]
+        # return days
         lst=[]
         days=0
         for i in range(len(plantTime)):
             lst.append((growTime[i],plantTime[i]))
         lst.sort()
+        lst.reverse()
+        days=planting=0
         for i in range(len(lst)):
-            days=max(days,lst[i][0])+lst[i][1]
+            planting+=lst[i][1]
+            days=max(days,lst[i][0]+planting)
         return days
